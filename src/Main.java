8a71a3e54851ce1868c3a74148f0dc1006b8ba9b
mertugras <mertugras5@gmail.com>
2021-04-24 23:02:10 +0300

    yeni değişiklikler

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("mert");
+
+
+        Product product1 = new Product(1, "Asus", 5.5, "mert");
+        Product product2 = new Product(2, "Hp", 5.5, "mert");
+        Product product3 = new Product(3, "Sony", 5.5, "mert");
+
+        Product[] products = {product1, product2, product3};
+
+        for (Product product : products) {
+            System.out.println(product.getName());
+        }
+
+        Category category1=new Category();
+        category1.id=1;
+        category1.name="Bilgisayar";
+
+        Category category2=new Category();
+        category2.id=2;
+        category2.name="Ev/Bahçe";
+
+
+
+
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,47 @@
 public class Product {
 
-    int id;
-    String Name;
-    double unitPrice;
-    String detail;
+    private int id;
+    private String Name;
 
+    public Product(int id, String name, double unitPrice, String detail) {
+        this.id = id;
+        Name = name;
+        this.unitPrice = unitPrice;
+        this.detail = detail;
+    }
+
+    private double unitPrice;
+    private String detail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
 }
